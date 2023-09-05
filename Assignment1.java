63e15d53826ef185e19787ce174bcc5c7f4b8477
45kiru <pokhrel.kiran14@gmail.com>
2023-09-05 23:29:35 +0545

    fix issues

--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public static Assignment1
+public class Assignment1
 {
     public static void main(String[] args)
     {
         // F1: allows the user to input the assignment name
-        Scanner scanner = new Scanner(); // import scanner to allow user input
+        Scanner scanner = new Scanner(System.in); // import scanner to allow user input
 
         System.out.print("Enter the assignment name: ");
         String assignmentName = scanner.nextLine();
