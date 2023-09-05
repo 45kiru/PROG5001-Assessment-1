cfa8ac22721bf8f206f6cc37a264da5bb68a3da8
45kiru <pokhrel.kiran14@gmail.com>
2023-09-05 22:52:33 +0545

    allows the user to input the assignment name

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,13 @@
-public static Assignment1{
-    public static void main(String[] args){
-        
+import java.util.Scanner;
+
+public static Assignment1
+{
+    public static void main(String[] args)
+    {
+        // F1: allows the user to input the assignment name
+        Scanner scanner = new Scanner(); // import scanner to allow user input
+
+        System.out.print("Enter the assignment name: ");
+        String assignmentName = scanner.nextLine();
     }
 }
