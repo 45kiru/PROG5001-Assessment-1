d9e381a676e82eb61a451eaf025a425ac7cba27d
45kiru <pokhrel.kiran14@gmail.com>
2023-09-05 23:08:06 +0545

    allows the user to input & if invalid re-enter

--- Assignment1.java
+++ Assignment1.java
@@ -9,5 +9,25 @@ public static Assignment1
 
         System.out.print("Enter the assignment name: ");
         String assignmentName = scanner.nextLine();
+
+        // F2: allows the user to input the assignment grade
+        int[] studentMarks = new int[30];
+
+        for (int i = 0; i < 30; i++) // assume that there are 30 students in the unit
+        {
+            do
+            {
+                System.out.print("Enter mark for Student " + (i + 1) + ": ");
+                int mark = scanner.nextInt();
+                if((mark >= 0) && (mark <= 30)) // must accept only valid mark (i.e. between 0 and 30) inputs by users.
+                {
+                    studentMarks[i] = mark;
+                    break;
+                } else {
+                // F3: allows the user to re-enter the assignment grade if the input is invalid
+                    System.out.println("Invalid input!\nPlease enter a mark between 0 and 30.");
+                }
+            } while (true);
+        }
     }
 }
